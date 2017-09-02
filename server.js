216cc5c03ae709bd9e9c735843e499c38ff499a0
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-09-02 13:30:57 +0530

    Added POST method to /todos route

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 var http = require ("http");
 var fs = require("fs");
+var qs = require("querystring");
 
+var ejs = require("ejs");
 var port = 3000;
 
+var todos = [];
+
+for(let i = 0; i <= 10; i++) {
+    todos.push({
+        id: i,
+        title: "todo " + i,
+        completed: false
+    })
+}
+
+console.log(todos);
+
 const server = http.createServer(function (req, res) {
-    if (req.url == "/")  {
-        fs.readFile("public/index.html", function (err, page) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(page);
-        })
+    console.log(req.url);
+    switch(req.url) {
+        case "/":
+            ejs.renderFile("./public/index.ejs", {todos: todos}, null, function (err, str) {
+                res.end(str);
+            });
+            break;
+        case "/todos":
+            if (req.method == "POST") {
+                var postedData = "";
+                req.on("data", function (chunk) {
+                    console.log(chunk);
+                    postedData += chunk;
+                });
+                req.on("end", function () {
+                    var todo = qs.parse(postedData);
+                    todo.completed = false;
+                    todo.id = todos.length;
+
+                    console.log(todo);
+
+                    todos.push(todo);
+                    res.writeHead(302, {"Location": "/"});
+                    res.end();
+                    
+                })
+            }
+            
+            break;
     }
 });
 
